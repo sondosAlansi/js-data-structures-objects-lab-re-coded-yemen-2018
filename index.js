a23adf00f8267990c9810586a87ba1a1e064fa4b
sondos alansi <sondos.alansi@yahoo.com>
2018-09-05 18:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   return driver;
   
 }
-function deleteFromDriverByKey(){
+function deleteFromDriverByKey(driver, key){
   
 }
